26b72d74d3b1bd287882245783e6b97804ecf5a7
meenahoda <meena.hoda@wmfs.net>
2021-11-16 08:51:28 +0000

    test: try removing pg plugin

--- test/fixtures/tymly-paths.js
+++ test/fixtures/tymly-paths.js
@@ -7,7 +7,7 @@ module.exports = {
     require.resolve('@wmfs/tymly-test-helpers/plugins/mock-os-places-plugin'),
     require.resolve('@wmfs/tymly-test-helpers/plugins/allow-everything-rbac-plugin'),
     require.resolve('@wmfs/tymly-cardscript-plugin'),
-    require.resolve('@wmfs/tymly-pg-plugin')
+    // require.resolve('@wmfs/tymly-pg-plugin')
   ],
   blueprintPaths: [
     path.resolve(__dirname, '../fixtures/blueprints/welcome-blueprint')

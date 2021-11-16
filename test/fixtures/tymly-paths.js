6e33c40941edead56344c879b6b5cbd48e8bc692
meenahoda <meena.hoda@wmfs.net>
2021-11-16 08:53:10 +0000

    fix: revert circle config, we don't need pg plugin

--- test/fixtures/tymly-paths.js
+++ test/fixtures/tymly-paths.js
@@ -6,8 +6,7 @@ module.exports = {
     require.resolve('@wmfs/tymly-test-helpers/plugins/mock-solr-plugin'),
     require.resolve('@wmfs/tymly-test-helpers/plugins/mock-os-places-plugin'),
     require.resolve('@wmfs/tymly-test-helpers/plugins/allow-everything-rbac-plugin'),
-    require.resolve('@wmfs/tymly-cardscript-plugin'),
-    // require.resolve('@wmfs/tymly-pg-plugin')
+    require.resolve('@wmfs/tymly-cardscript-plugin')
   ],
   blueprintPaths: [
     path.resolve(__dirname, '../fixtures/blueprints/welcome-blueprint')

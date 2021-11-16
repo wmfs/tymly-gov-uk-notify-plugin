f8bfc91bd6d01f966fe1f3e4d757ed9420974929
meenahoda <meena.hoda@wmfs.net>
2021-11-16 08:45:30 +0000

    test: add extract plugin paths

--- test/fixtures/tymly-paths.js
+++ test/fixtures/tymly-paths.js
@@ -2,7 +2,12 @@ const path = require('path')
 
 module.exports = {
   pluginPaths: [
-    path.resolve(__dirname, '../../lib')
+    path.resolve(__dirname, '../../lib'),
+    require.resolve('@wmfs/tymly-test-helpers/plugins/mock-solr-plugin'),
+    require.resolve('@wmfs/tymly-test-helpers/plugins/mock-os-places-plugin'),
+    require.resolve('@wmfs/tymly-test-helpers/plugins/allow-everything-rbac-plugin'),
+    require.resolve('@wmfs/tymly-cardscript-plugin'),
+    require.resolve('@wmfs/tymly-pg-plugin')
   ],
   blueprintPaths: [
     path.resolve(__dirname, '../fixtures/blueprints/welcome-blueprint')
